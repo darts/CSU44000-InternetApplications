8447b194ce3388b8d5389f4f772bb6aff93e14bb
darts <darts@tcd.ie>
2020-12-05 17:38:04 +0000

    remove async error handling because that's complicated and unmaintainable

--- Assignment-2/server.js
+++ Assignment-2/server.js
@@ -1,7 +1,6 @@
 const SERVER_PORT = 8001;
 const express = require("express");
 const app = express();
-const cors = require("cors");
 const AWS = require("aws-sdk");
 AWS.config.update({
     region: 'us-east-1'
@@ -33,17 +32,7 @@ let dbParams = {
     BillingMode: "PAY_PER_REQUEST"
 };
 
-app.use(cors()); //Allow cross origin requests for local development
 app.use(express.static('public')); //serve the webpage
-
-//this feature is coming natively in Express 5
-//Express 5 is currently in Alpha so this will do for now
-//catches async errors and passes them to the error handling fn defined at the end
-function asyncErrWrapper(asyncFun) {
-    return function (req, res, next) {
-        asyncFun(req, res, next).catch(next);
-    };
-}
 
 //returns a promise object from the s3 bucket
 function getFromS3Bucket() {
@@ -89,20 +78,20 @@ function queryDB(year, title) {
     return docClient.query(queryParams).promise();
 }
 
-app.get("/movie", asyncErrWrapper(async (req, res) => {
+app.get("/movie", async (req, res) => {
     let dbRes = await queryDB(Number(req.query.year), req.query.title);
     res.send(dbRes.Items);
-}));
+});
 
-app.delete("/database", asyncErrWrapper(async (_, res) => {
+app.delete("/database", async (_, res) => {
     console.log("Deleting Database");
     await dynamoDB.deleteTable({ TableName: TABLE }).promise();
     await dynamoDB.waitFor("tableNotExists", {TableName: TABLE}).promise();
     console.log("Database deleted");
     res.status(200).send("Database deleted");
-}));
+});
 
-app.post("/database", asyncErrWrapper(async (_, res, next) => {
+app.post("/database", async (_, res, next) => {
     console.log("Creating Database");
     await dynamoDB.createTable(dbParams).promise();
     await dynamoDB.waitFor("tableExists", {TableName: TABLE}).promise();
@@ -110,16 +99,7 @@ app.post("/database", asyncErrWrapper(async (_, res, next) => {
     await Promise.all(writeToDynamo(response_json));
     console.log("Database Created!");
     res.status(200).send("Database Created!");
-}));
-
-//handles async errors
-app.use((err, req, res, next) => {
-    console.error(err);
-    if (err?.message.startsWith("Table already exists"))
-        return res.status(210).send("Table already exists");
-    else if (err?.message.startsWith("Requested resource not found: Table:"))
-        return res.status(210).send("Table already deleted");
-    res.status(500).send(err);
 });
 
 app.listen(SERVER_PORT, () => console.log(`Server running on port: ${SERVER_PORT}`));
+
